7e30ec790aa0b17b4310a388eb4439ad1936eb2b
microdot178 <microdot178@tuta.io>
2020-05-25 01:49:34 +0000

    reedit

--- accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from accounts.views import *
-from . import views
-
-
-urlpatterns = [
-    #path('users/all/', UserListView.as_view()),
-    #path('users/update/', UserUpdateView.as_view()),
-]
